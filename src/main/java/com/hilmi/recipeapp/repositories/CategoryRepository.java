50428165e8681e76ecd6b4876793836463925572
Ahmad Nurhasan Hilmi <nurhasanhilmi@gmail.com>
2022-10-20 21:49:23 +0700

    Add findByName abstact method

--- src/main/java/com/hilmi/recipeapp/repositories/CategoryRepository.java
+++ src/main/java/com/hilmi/recipeapp/repositories/CategoryRepository.java
@@ -4,6 +4,10 @@ import com.hilmi.recipeapp.model.Category;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CategoryRepository extends CrudRepository<Category, Long> {
+
+    Optional<Category> findByName(String name);
 }
--- src/main/java/com/hilmi/recipeapp/repositories/UnitOfMeasureRepository.java
+++ src/main/java/com/hilmi/recipeapp/repositories/UnitOfMeasureRepository.java
@@ -4,6 +4,10 @@ import com.hilmi.recipeapp.model.UnitOfMeasure;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long> {
+
+    Optional<UnitOfMeasure> findByName(String name);
 }
